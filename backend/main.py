14f8faf1e499f6142257e175bc7a58d50bbeb04b
WiNg4205 <105526030+WiNg4205@users.noreply.github.com>
2024-07-23 17:46:27 +1000

    basic setup

---
+++ backend/main.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+df = pd.read_csv('rym_top_5000_all_time.csv')
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="text-3xl font-bold underline">Hello!</div>
+      <input class="bg-gray-200" type="text" placeholder="Search..."></input>
+    </div>
+  )
+}
+
+export default App
